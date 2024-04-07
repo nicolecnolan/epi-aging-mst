4e97ed3efa4c8634c0fa3eaf46c3c7bd58d48724
Nicole Nolan <nicolecnolan@gmail.com>
2024-04-07 14:31:33 -0700

    use python gzip instead of wsl

--- download-data.bat
+++ download-data.bat
@@ -2,8 +2,9 @@ curl --create-dirs http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz -
 curl --create-dirs http://yann.lecun.com/exdb/mnist/train-labels-idx1-ubyte.gz -o dataset/MNIST/raw/train-labels-idx1-ubyte.gz
 curl --create-dirs http://yann.lecun.com/exdb/mnist/t10k-images-idx3-ubyte.gz -o dataset/MNIST/raw/t10k-images-idx3-ubyte.gz
 curl --create-dirs http://yann.lecun.com/exdb/mnist/t10k-labels-idx1-ubyte.gz -o dataset/MNIST/raw/t10k-labels-idx1-ubyte.gz
-cd dataset\MNIST\raw
-wsl gunzip train-images-idx3-ubyte.gz
-wsl gunzip train-labels-idx1-ubyte.gz
-wsl gunzip t10k-images-idx3-ubyte.gz
-wsl gunzip t10k-labels-idx1-ubyte.gz
+
+py -m gzip -d dataset\MNIST\raw\train-images-idx3-ubyte.gz
+py -m gzip -d dataset\MNIST\raw\train-labels-idx1-ubyte.gz
+py -m gzip -d dataset\MNIST\raw\t10k-images-idx3-ubyte.gz
+py -m gzip -d dataset\MNIST\raw\t10k-labels-idx1-ubyte.gz
+
